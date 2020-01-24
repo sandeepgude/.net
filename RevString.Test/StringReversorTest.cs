3be1e96379e8459e7eae040b17e0bf28323ed843
sgude4 <sandeep.kcgude@gmail.com>
2020-01-24 16:39:45 -0700

    Version 1 - Added comments

--- RevString.Test/StringReversorTest.cs
+++ RevString.Test/StringReversorTest.cs
@@ -14,12 +14,17 @@ namespace RevString.Test
         public void ReverseString_Always_ReturnsExpectedValue()
         {
             string input = "AB1;C2*D";
+
+            // Mocking the database using library Moq
             var mockRepository = new Mock<IRepository>(MockBehavior.Strict);
+            //setting the input value to the Repository
             mockRepository.Setup(x => x.GetString()).Returns(input);
+            // Creating an instance for String Reversor Class
             var SystemUnderTest = new StringReversor(mockRepository.Object);
 
 
             string expectedoutput = "D2C;1B*A";
+            // Calling Reverse_string function to Test the functionality
             var Result = SystemUnderTest.Reverse_String();
             Assert.AreEqual(expectedoutput, Result);
 
